e18f87fa36a20f17e038d052776a184878e9339c
Ashutosh Kumar <itsray650@gmail.com>
2025-06-06 18:05:45 +0530

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import { Login } from './component/Login_Page'
+import { ChatApp } from './component/Home'
 import { Routes,Route } from 'react-router-dom'
 function App() {
   
@@ -9,7 +10,7 @@ function App() {
     <>
     <Routes>
     <Route path="/login" element={<Login />} />
-    
+    <Route path="/home" element={<ChatApp/>}/>
      </Routes>
     </>
   )
