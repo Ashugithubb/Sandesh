41bb8ce7341a00dabaebc76845ba779f9886896b
Ashutosh Kumar <114423632+Ashugithubb@users.noreply.github.com>
2025-10-19 23:35:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Embedder-Policy': 'require-corp'
     }
-  },
-  build: {
-    outDir: 'dist', y
   }
 })
